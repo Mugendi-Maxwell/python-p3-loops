35a310cd379305ce4502bc926e46d78d21a932ca
Maxi-arch <mxmu025@gmail.com>
2024-11-26 15:14:08 +0300

    1stcommit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+   counter=10
+   while counter > 0:
+       print(counter)
+       counter +=-1
+   print("Happy New Year!")    
 def square_integers(int_list):
-    # code goes here!
-    pass
+    for i in int_list:
+        print(i*i)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    nums = 1  
+    while nums <= 100:  
+        if nums % 3 == 0 and nums % 5 == 0:
+            print("FizzBuzz")
+        elif nums % 3 == 0:
+            print("Fizz")
+        elif nums % 5 == 0:
+            print("Buzz")
+        else:
+            print(nums)
+        nums += 1  
+
+happy_new_year()
+square_integers(int_list=[12,13,14,25])
+fizzbuzz()
+
+  
+   
